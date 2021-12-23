07e32b0fcd868d90dafa6615f4be185628d9a1cc
Nilo Ferreira <nsmf.gnf@gmail.com>
2021-12-22 21:15:28 -0300

    add decorator

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,10 @@ const serverHTTP = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req: Request, res: Response) => {
+  throw new Error('teste');
+});
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err) {
     res.json({ message: err.message });
